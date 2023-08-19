d4817f87cae0f7d4bc67bad4acb15fcf40c68ef4
Devarsh Makwana <devarshppsv@gmail.com>
2023-08-19 19:43:27 +0530

    added styles in index.css files and structured all components and integrate with app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Servecies from './components/servecies/Servecies'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
     <div>
-     App 
+        <Header></Header>
+        <Nav></Nav>
+        <About></About>
+        <Experience></Experience>
+        <Servecies></Servecies>
+        <Portfolio></Portfolio>
+        <Testimonials></Testimonials>
+        <Contact></Contact>
+        <Footer></Footer>
     </div>
   )
 }
