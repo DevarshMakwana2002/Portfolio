2815b9a8057ef4366d648231152681bc5baad485
Devarsh Makwana <devarshppsv@gmail.com>
2023-08-22 22:38:04 +0530

    Modifing experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,11 +1,86 @@
-import React from 'react'
+import React from "react";
+import { BsPatchCheckFill } from "react-icons/bs";
+import './Experience.css'
 
 const Experience = () => {
   return (
     <section id="experience">
-     Experience 
+      <h5>What skills I Have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>CSS</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>JavaScriipt</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>ReactJs</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>VueJs</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>Next</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>NodeJs</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>Nest</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>Django</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            {/* <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>ReactJs</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>VueJs</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>Nest</h4>
+              <small className="text-light">Experienced</small>
+            </article> */}
+          </div>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
